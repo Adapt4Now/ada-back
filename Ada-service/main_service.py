efe0ad5669114cb1a7eb9618ef738a1d0411f2b0
ferretvl <gerasimetsav@gmail.com>
2024-03-11 21:47:05 +0200

    1st version using vercel provider

--- Ada-service/main_service.py
+++ Ada-service/main_service.py
@@ -1,1 +1,27 @@
-print(111)
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"I think that should be logged"}
+
+
+@app.get("/simple")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/input/{message}")
+def read_item(message: str):
+    return {"Input received:": message}
+
+
+def run():
+    uvicorn.run(app, host="ada-back.vercel.app", port=443, log_level="info")
+
+
+if __name__ == "__main__":
+    run()
